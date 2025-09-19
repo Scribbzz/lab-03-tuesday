4726d186227da84fbcdcdde1fc426c31c077b81a
Ahaan Mandan <95626551+Scribbzz@users.noreply.github.com>
2025-09-19 15:29:51 -0600

    Update City.java

--- code/app/src/main/java/com/example/listycitylab3/City.java
+++ code/app/src/main/java/com/example/listycitylab3/City.java
@@ -1,30 +1,16 @@
 package com.example.listycitylab3;
 
-import java.io.Serializable;
-
-public class City implements Serializable {
+public class City {
     private String name;
     private String province;
-
     public City(String name, String province) {
         this.name = name;
         this.province = province;
     }
-
     public String getName() {
         return name;
     }
-
     public String getProvince() {
         return province;
-    }
-
-    // 🔹 Add setters
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public void setProvince(String province) {
-        this.province = province;
     }
 }
